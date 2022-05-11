bbf22a2647595f399ecb160cd6a614bd0ded6328
too0ky <36736058+too0ky@users.noreply.github.com>
2022-05-11 11:13:16 +0200

    Update About.js
    Update content

--- src/contents/About.js
+++ src/contents/About.js
@@ -9,16 +9,11 @@ class About extends Component {
             <div className="condiv">
             <h1 className="subtopic">I'm Trang Ngo</h1>
             <h3 className='quote'>"Nothing is neither good nor bad but thinking makes it so"</h3>
-            <h2>Python | Javascript | React Developer</h2>
+            <h2>Python| Django | Javascript | React Developer</h2>
             <br></br>
-            <p>Hi! I’m Trang! Let me introduce myself: I am a self-taught developer (can I say that yet?), looking to dip my toes into the wonderful world of IT. For as long as I can remember, I have always been curious about what I will do as an adult. Well, I am an adult now, and I can’t honestly say what that will be! But I do know I am interested in creating something wonderful.</p>
-            <br></br>
-            <p>This past year, I have been teaching myself Web Development and Data Analysis through Coursera, FreeCodeCamp, practicing on my personal projects and learning alongside many tutorials. 
-            Data Analysis because I believe it’s necessary to understand the information around you. To make informed decisions based on facts and data. Web Development because it’s wonderful and rewarding to create something beautiful from nothing. “Hello World” is the beginning of an exciting adventure.</p>
-            <br></br>
-            <p>I am a firm believer that if you set your mind on something, you will achieve it. Everything is difficult in the beginning, but if you are persistent, you will achieve your goals. </p>
-            <br></br>
-            <p>Thank you for taking the time to look at my portfolio, and I hope you enjoy the things you see! 
+            <p>I am a fervent admirer of data analysis, visualization, and web development with a flair for acquiring technical languages. Coupled with excellent critical thinking skills and a unique sense of humor, I am able to achieve deliverables efficiently with a smile. As an educator for the past six years in different international settings, I am an effective communicator and cheerleader to those around me. Finally, my passion for sustainability propels me towards conscious projects with significant impact on our global environment.
+            <br>
+            Thank you for taking the time to look at my portfolio, and I hope you enjoy the things you see! 
             </p>
             
             </div>
